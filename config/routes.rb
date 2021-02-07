523767d93d2977e82e43437fd3d6fcd0703a18f2
Syed Mona Javed <monajaved93@gmail.com>
2021-02-06 22:39:09 -0800

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users 
+  resources :posts
+
+  resources :users do 
+    resources :posts, only: [:index, :show, :new, :destroy, :edit, :update, :create]
+  end
+ 
+
+
+
+  root 'static#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
